480cf99f1ca6aec4800228c23e69ddee0b3f859b
samar Manjeshwar <samar@nair-assoc.com>
2021-10-27 22:41:12 -0400

    Simple index endpoint load

--- pl_fixtures.py
+++ pl_fixtures.py
@@ -1,2 +1,13 @@
 from flask import Flask, request
+import json
+import requests
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+  return json.dumps({"name": "Premier League Fixtures"})
+
+if __name__ == '__main__':
+  app.run(debug=True, host = '127.0.0.1', port = 8000)
